4fce84466b4800e33367c0de072a9366c0da879f
kgolec93 <kamil.golec@gmail.com>
2019-06-17 22:47:18 +0200

    Portfolio sketch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const itemList = [
+  {name: 'digitalARCH.pl', framework: 'jQuery'},
+  {name: 'kursyblendera.pl', framework: 'jQuery'},
+  {name: 'Weather Forecast', framework: 'React'},
+  {name: 'Chat Module', framework: 'React // Firebase'},
+  {name: 'Projectizer', framework: 'React + Redux // Firebase'},
+  // {name: 'Money Saver', framework: 'React + Redux // Firebase'},
+]
+
+class GalleryItem extends Component {
+  render() {
+    return (
+        <div className='galleryItem'>
+          <p>{this.props.name}</p>
+          <p>{this.props.framework}</p>
+        </div>
+    )
+  }
 }
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <p>PORTFOLIO</p>
+        <main>
+          <div className='galleryContainer'>
+            <div>
+              Arrow left
+            </div>
+              <div className='galleryBorder'>
+                <div className="galleryItemsContainer">
+                  {itemList.map((i)=>{
+                    return (
+                      <GalleryItem 
+                        name={i.name}
+                        framework={i.framework}
+                      />
+                    )
+                  })}
+                </div>
+              </div>
+
+            <div>
+              Arrow Right
+            </div>
+          </div>
+
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
